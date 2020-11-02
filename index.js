c2850ad4d85bda870fc8373460b7a4c31b94d955
3kingz <victorkenneth95@gmail.com>
2020-11-02 09:42:19 +0100

    code refactor

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 function formatNaira() {
   const nairaSymbol = "\u{020A6}";
   let sum = 0;
-  let value;
-  if (arguments.length === 0) {
-    return nairaSymbol + 0;
-  }
   for (let i = 0; i < arguments.length; i++) {
     sum += arguments[i];
   }
-  value = nairaSymbol + new Intl.NumberFormat("en-US").format(sum);
-  return value;
+  return nairaSymbol + new Intl.NumberFormat("en-US").format(sum);
 }
 
 module.exports = formatNaira;
